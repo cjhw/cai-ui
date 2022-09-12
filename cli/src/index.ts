c509a7a0a327f3c0faa08c2ee3d10408b591552e
caijianhaoa <1784581164@qq.com>
2022-09-12 23:47:49 +0800

    开发命令脚本

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello')
+import { Command } from 'commander'
+
+const cmd = new Command()
+
+cmd
+  .command('create')
+  .description('创建一个组件模板或配置文件')
+  // 添加命令参数-t | --type,<type>表明为必选参数
+  .option('-t --type <type>', '创建类型，可选值：component, lib-entry')
+  // 注册回调函数
+  .action(args => {
+    console.log(args)
+  })
+
+// 执行命令行参数的解析
+cmd.parse()
