19e35455aca0a7099e429990f92a039d58176b7a
caijianhaoa <1784581164@qq.com>
2022-09-12 21:27:53 +0800

    使用vitest搭建测试环境

---
+++ src/components/Test.test.ts
@@ -0,0 +1,9 @@
+import Test from './Test'
+import { render } from '@testing-library/vue'
+
+test('Test.tsx should work', () => {
+  // 渲染组件
+  const { getByText } = render(Test)
+  // 断言输出结果
+  getByText('test: 0')
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
+/// <reference types="vitest"/>
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx({})]
+  plugins: [vue(), vueJsx({})],
+  test: {
+    // 像jest一样的api
+    globals: true,
+    // 模拟dom环境
+    environment: 'happy-dom',
+    // 支持tsx
+    transformMode: {
+      web: [/.[tj]sx$/]
+    }
+  }
 })
